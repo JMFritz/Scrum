c27fd9894f0bab749d6483d4c2af4e14386c7286
Jun Fritz <jun.fritz@gmail.com>
2017-08-15 19:45:06 -0700

    add tools controller and functionality to create new tool instance

---
+++ src/Scrum/Controllers/ToolsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Scrum.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Scrum.Controllers
+{
+    public class ToolsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ToolsController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Tools.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Tool tool)
+        {
+            _db.Tools.Add(tool);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

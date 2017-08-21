35973113c11a513ab0a368d3ed6ca5493d375522
Jun Fritz <jun.fritz@gmail.com>
2017-08-21 15:46:42 -0700

    add update type class for update specification

---
+++ src/Scrum/Models/UpdateType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scrum.Models
+{
+    [Table("UpdateTypes")]
+    public class UpdateType
+    {
+        [Key]
+        public int UpdateTypeId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Update> Updates { get; set; }
+    }
+}
